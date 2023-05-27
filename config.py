a8c9d34f8eb49ffc423de5ef8fadf238c0d49eca
seilige <oneMillionDeffect@gmail.com>
2023-05-27 10:00:02 +0300

    a

---
+++ config.py
@@ -0,0 +1,13 @@
+token: str = "MTEwNTcxOTcxNjMzNDUzNDc2Nw.GKfU6Z.yqZOv3Dzh7nL02BM0cYKt4F5FBLRam9g81ntos"
+
+postId: int = 1097468427079266315
+
+mutedName: str = "Muted"
+
+roles: dict[str: int] = {
+    "👈": 1100777311235739708,
+    "👉": 1100777356408401990,
+    "👆": 1100777394480099328
+}
+
+mainRoles = ["Программист"]
