2222b1876d6f21c048b144aa2aa5d14cf1427c7d
seilige <89296220+seilige@users.noreply.github.com>
2023-05-27 09:02:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-token: str = "MTEwNTcxOTcxNjMzNDUzNDc2Nw.GKfU6Z.yqZOv3Dzh7nL02BM0cYKt4F5FBLRam9g81ntos"
+token: str = ""
 
-postId: int = 1097468427079266315
+postId: int = 0
 
-mutedName: str = "Muted"
+mutedName: str = ""
 
-roles: dict[str: int] = {
-    "👈": 1100777311235739708,
-    "👉": 1100777356408401990,
-    "👆": 1100777394480099328
-}
+roles: dict[str: int] = {}
 
-mainRoles = ["Программист"]
+mainRoles = []
